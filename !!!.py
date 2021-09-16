3a367f9df91fc747c75b0ad214da85bf26543918
thanaphat.kar <thanaphat.kar@CPFHP-CG8207RKP>
2021-09-16 22:43:30 +0700

    1

--- !!!.py
+++ !!!.py
@@ -1,6 +1,8 @@
 import re
 data1 = open('aa.txt','r').read()
-find1 = re.findall("\d+\.\d+ USD",data1)
+find1 = re.findall("\d+.\d+ USD",data1)
+#find1 = re.findall("\d+\.\d+ USD",data1)
 #re.findall("\d+\.*\d* USD", text)
+# print(data1)
 for i in find1:
     print(i)
--- !!.py
+++ !!.py
@@ -1,3 +1,4 @@
+import re
 data1 = open('aa.txt','r')
 
 # for n in data1:
@@ -45,16 +46,23 @@ for i in data1:
     # [1, 3]
     # [1]
     # []
-
+    # for j in list_out_conv:
+    #     print(j)
+    #     # key = re.findall("USD.",j)
+    #     # print(key)
     for j in list_float_num:
         key=list_out_conv[j+1].strip('').strip('\n').replace('.','').upper() #USD #ใช้ regex ดีกว่า
+        #key = re.findall("USD",list_out_conv)
         if key in dict_collect:
-            dict_collect[list_out_conv[j+1].strip('').strip('\n').replace('.','').upper()] += list_out_conv[j]
+            #dict_collect[list_out_conv[j+1].strip('').strip('\n').replace('.','').upper()] += list_out_conv[j]
+            dict_collect[key] += list_out_conv[j]
         else:
-            dict_collect[list_out_conv[j+1].strip('').strip('\n').replace('.','').upper()] = list_out_conv[j]
+            #dict_collect[list_out_conv[j+1].strip('').strip('\n').replace('.','').upper()] = list_out_conv[j]
+            dict_collect[key] = list_out_conv[j]
 
-    print(dict_collect)
-    # for cur in curren: #USD in USD
-    #     if cur in dict_collect: #100,700,300.56
-    #         print(dict_collect[cur],cur)
+
+    #print(dict_collect)
+    for cur in curren: #USD in USD
+        if cur in dict_collect: #100,700,300.56
+            print(dict_collect[cur],cur)
     #         #200,USD
